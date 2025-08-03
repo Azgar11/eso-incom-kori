d10f15e348ac356ea09fa9c0a3fef7e03d16580c
Azgar11 <lamiya2001ali@gmail.com>
2025-08-03 11:49:21 +0600

    Create App.js

---
+++ public/public/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>এসো ইনকাম করি</h1>
+      <p>রেফার করুন আর ইনকাম করুন!</p>
+    </div>
+  );
+}
+
+export default App;
